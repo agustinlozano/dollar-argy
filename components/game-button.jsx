ef70cf5b676856275ca74f2620f80deb02df7d76
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-14 19:15:32 -0300

    Add game controls

--- components/game-button.jsx
+++ components/game-button.jsx
@@ -2,9 +2,10 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-export default function GameButton({ className, children, ...props }) {
+export default function GameButton({ id, className, children, ...props }) {
   return (
     <Button
+      id={id}
       variant="outline"
       className={cn(
         "w-full h-10 border border-border shadow-[3px_5px_0px_0px_rgba(0,0,0,0.75)]",
--- components/game-controls.jsx
+++ components/game-controls.jsx
@@ -5,12 +5,22 @@ export default function Controls({ onMove }) {
   return (
     <div className="absolute bottom-5 w-full flex justify-center items-end">
       <div className="grid grid-cols-3 gap-2 w-[160px]">
-        <GameButton className="col-span-3" onClick={() => onMove("forward")}>
+        <GameButton
+          id="forward"
+          className="col-span-3"
+          onClick={() => onMove("forward")}
+        >
           ▲
         </GameButton>
-        <GameButton onClick={() => onMove("left")}>◀</GameButton>
-        <GameButton onClick={() => onMove("backward")}>▼</GameButton>
-        <GameButton onClick={() => onMove("right")}>▶</GameButton>
+        <GameButton id="left" onClick={() => onMove("left")}>
+          ◀
+        </GameButton>
+        <GameButton id="backward" onClick={() => onMove("backward")}>
+          ▼
+        </GameButton>
+        <GameButton id="right" onClick={() => onMove("right")}>
+          ▶
+        </GameButton>
       </div>
     </div>
   );
