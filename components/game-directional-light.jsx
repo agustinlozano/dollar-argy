f01fb545ea5b93231f3c119e655730f255069a44
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-16 20:40:31 -0300

    Refactor PlayerDirectionalLight component for improved lighting management
    - Removed playerPosition dependency and adjusted light positioning logic.
    - Introduced environment-based helper for directional light in development mode.
    - Added a second directional light for enhanced scene illumination.
    - Updated light intensity and shadow settings for better visual quality.

--- components/game-directional-light.jsx
+++ components/game-directional-light.jsx
@@ -1,39 +1,45 @@
 import { useRef, useEffect } from "react";
 import { useHelper } from "@react-three/drei";
 import * as THREE from "three";
+import { enviroment } from "@/lib/env-vars";
 
 // Game environment lights
-export function PlayerDirectionalLight({ playerPosition }) {
-  const dirLightRef = useRef();
+export function PlayerDirectionalLight() {
+  const dirLightRef2 = useRef();
   const targetRef = useRef();
 
   // Usar el hook useHelper de drei
-  useHelper(dirLightRef, THREE.DirectionalLightHelper, 100, "red");
+  if (enviroment === "development") {
+    useHelper(dirLightRef2, THREE.DirectionalLightHelper, 100, "blue");
+  }
 
   useEffect(() => {
-    if (dirLightRef.current && playerPosition && targetRef.current) {
-      // Posicionamos la luz en relación con el jugador
-      dirLightRef.current.position.set(
-        playerPosition.x - 200,
-        playerPosition.y - 200,
-        400
-      );
-      dirLightRef.current.up.set(0, 0, 1);
-
-      // Actualizamos el target para que apunte hacia el jugador
-      targetRef.current.position.set(playerPosition.x, playerPosition.y, 0);
-
-      // Aseguramos que la luz apunta al target
-      dirLightRef.current.target = targetRef.current;
-      dirLightRef.current.target.updateMatrixWorld();
+    if (dirLightRef2.current) {
+      dirLightRef2.current.target.position.set(0, 1300, 0);
+      dirLightRef2.current.target.updateMatrixWorld();
     }
-  }, [playerPosition]);
+  }, []);
 
   return (
     <>
       <directionalLight
-        ref={dirLightRef}
-        intensity={1}
+        position={[-200, -200, 400]}
+        intensity={0.5}
+        castShadow
+        shadow-mapSize-width={4096}
+        shadow-mapSize-height={4096}
+        shadow-camera-left={-800}
+        shadow-camera-right={800}
+        shadow-camera-top={800}
+        shadow-camera-bottom={-800}
+        shadow-camera-near={50}
+        shadow-camera-far={800}
+      />
+
+      <directionalLight
+        ref={dirLightRef2}
+        position={[200, 1000, 400]}
+        intensity={0.5}
         castShadow
         shadow-mapSize-width={4096}
         shadow-mapSize-height={4096}
