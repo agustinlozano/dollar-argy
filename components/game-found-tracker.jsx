27f1d9208acd9be545954125e682b41d699990d6
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-18 20:08:10 -0300

    Improve found amount readability

--- components/game-found-tracker.jsx
+++ components/game-found-tracker.jsx
@@ -9,8 +9,14 @@ export function FoundTracker() {
   const found = getQuantity("gold_coins");
 
   return (
-    <div className="absolute top-5 right-[21rem]">
-      <div className="flex gap-x-4 justify-between items-center z-10">
+    <div className="absolute top-5 right-5">
+      <div className="relative flex gap-x-4 justify-between items-center z-10 px-4">
+        <div
+          className={cn(
+            "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
+            "rounded bg-accent-foreground/75 h-8 w-full flex items-center justify-center -z-10"
+          )}
+        />
         <div
           id="found-tacker-coin-icon"
           className={cn(
@@ -22,7 +28,7 @@ export function FoundTracker() {
 
         <div
           aria-label="amount"
-          className="flex items-center gap-2 font-bold text-accent"
+          className="flex items-center gap-2 font-bold text-accent text-lg"
         >
           <span>{found || 0}</span>
         </div>
