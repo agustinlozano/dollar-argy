993cdbc9eb0de163ea67efe085ce6ac4a16d6bfa
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-19 02:12:44 -0300

    Cast shadown on my M16 babe

--- components/game-weapon-m16.jsx
+++ components/game-weapon-m16.jsx
@@ -13,49 +13,49 @@ export function M16M1({ position = [0, 0, 0] }) {
   return (
     <group ref={group} position={position} scale={[1, 1, 1]}>
       {/* Stock */}
-      <mesh position={[-16, 0, 0]}>
+      <mesh position={[-16, 0, 0]} castShadow>
         <boxGeometry args={[10, 4, 2]} />
         <meshStandardMaterial color="#1a1a1a" />
       </mesh>
 
       {/* Receiver */}
-      <mesh position={[-5, 0, 0]}>
+      <mesh position={[-5, 0, 0]} castShadow>
         <boxGeometry args={[8, 4, 2]} />
         <meshStandardMaterial color="#2b2b2b" />
       </mesh>
 
       {/* Carry handle */}
-      <mesh position={[-5, 2.5, 0]}>
+      <mesh position={[-5, 2.5, 0]} castShadow>
         <boxGeometry args={[6, 1, 1]} />
         <meshStandardMaterial color="#1f1f1f" />
       </mesh>
 
       {/* Pistol grip */}
-      <mesh position={[-7.5, -3, 0]} rotation={[Math.PI / 6, 0, 0]}>
+      <mesh position={[-7.5, -3, 0]} rotation={[Math.PI / 6, 0, 0]} castShadow>
         <boxGeometry args={[1.2, 3, 1]} />
         <meshStandardMaterial color="#111" />
       </mesh>
 
       {/* Magazine */}
-      <mesh position={[-3, -2.5, 0]} rotation={[Math.PI / 10, 0, 0]}>
+      <mesh position={[-3, -2.5, 0]} rotation={[Math.PI / 10, 0, 0]} castShadow>
         <boxGeometry args={[1.5, 4, 1]} />
         <meshStandardMaterial color="#333" />
       </mesh>
 
       {/* Handguard */}
-      <mesh position={[6, 0, 0]}>
+      <mesh position={[6, 0, 0]} castShadow>
         <boxGeometry args={[14, 3.2, 2]} />
         <meshStandardMaterial color="#1a1a1a" />
       </mesh>
 
       {/* Front sight */}
-      <mesh position={[14, 2, 0]}>
+      <mesh position={[14, 2, 0]} castShadow>
         <boxGeometry args={[1, 3, 1]} />
         <meshStandardMaterial color="#1a1a1a" />
       </mesh>
 
       {/* Barrel */}
-      <mesh position={[25, 0.5, 0]} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh position={[25, 0.5, 0]} rotation={[Math.PI / 2, 0, 0]} castShadow>
         <cylinderGeometry args={[0.3, 0.3, 12, 12]} />
         <meshStandardMaterial color="#2a2a2a" />
       </mesh>
