9631d151f49e11122f392c9485f5704850f04b9b
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-19 15:55:31 -0300

    Add new colors for Grass terrain component

--- components/game-terrain-grass.jsx
+++ components/game-terrain-grass.jsx
@@ -10,19 +10,19 @@ export function Grass({ rowIndex }) {
       {/* Middle section */}
       <mesh receiveShadow position={[0, 0, 1.5]}>
         <boxGeometry args={[tilesPerRow * tileSize, tileSize, 3]} />
-        <meshLambertMaterial color="#e8f1f2" flatShading />
+        <meshLambertMaterial color="#60993e" flatShading />
       </mesh>
 
       {/* Left section */}
       <mesh position={[-tilesPerRow * tileSize, 0, 1.5]}>
         <boxGeometry args={[tilesPerRow * tileSize, tileSize, 3]} />
-        <meshLambertMaterial color="#C7C9C9" flatShading />
+        <meshLambertMaterial color="#065143" flatShading />
       </mesh>
 
       {/* Right section */}
       <mesh position={[tilesPerRow * tileSize, 0, 1.5]}>
         <boxGeometry args={[tilesPerRow * tileSize, tileSize, 3]} />
-        <meshLambertMaterial color="#C7C9C9" flatShading />
+        <meshLambertMaterial color="#065143" flatShading />
       </mesh>
     </group>
   );
