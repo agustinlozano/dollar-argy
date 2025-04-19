bddee648c74cb856f164229fdf265a99c3670077
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-18 22:53:15 -0300

    Add game inventory component

---
+++ components/game-inventory.jsx
@@ -0,0 +1,12 @@
+import { useInventoryStore } from "@/stores/useInventoryState";
+import { InventoryMenu } from "@/components/ui/inventory";
+
+export function GameInventory() {
+  const { items, spells } = useInventoryStore();
+
+  return (
+    <div className="absolute top-5 right-5 z-10">
+      <InventoryMenu items={items} spells={spells} />;
+    </div>
+  );
+}
