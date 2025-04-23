2a557cb8836729d8845103b1d75d0200114fa72b
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-23 16:08:55 -0300

    Add missing imports

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
 import { DollarArgyGame } from "@/components/game";
 
+import * as offensiveSpells from "@/lib/spells.offensives";
+import * as defensivesSpells from "@/lib/spells.defensives";
+import * as consumables from "@/lib/items.consumable";
+import * as magicItems from "@/lib/items.magic";
+
 export default function Home() {
+  console.log([
+    ...Object.values(offensiveSpells),
+    ...Object.values(defensivesSpells),
+  ]);
+
+  console.log([...Object.values(consumables), ...Object.values(magicItems)]);
   return (
     <main className="flex flex-col items-center justify-center">
       <DollarArgyGame />
