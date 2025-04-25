b82bc637a5272a33317e0360804513433c790e53
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-25 01:44:15 -0300

    Fit things better

--- components/ui/inventory/spell-grid.jsx
+++ components/ui/inventory/spell-grid.jsx
@@ -5,12 +5,10 @@ import SpellCard from "@/components/ui/inventory/spell-card";
 
 export default function SpellGrid({ spells, selectedSpell, onSelectSpell }) {
   return (
-    <div className="relative flex flex-col h-full">
-      <h2 className="text-xl font-bold mb-4 font-cormorant uppercase">
-        Spellbook
-      </h2>
-      <ScrollArea className="grow h-[450px] pr-4">
-        <div className="grid grid-cols-3 gap-2  select-none">
+    <div className="relative flex flex-col mt-3 h-full">
+      <h2 className="text-xl font-bold mb-1 font-cormorant">Spellbook</h2>
+      <ScrollArea className="grow h-[420px] pr-4">
+        <div className="grid grid-cols-3 gap-2 select-none">
           {spells.map((spell) => (
             <SpellCard
               key={spell.slug}
