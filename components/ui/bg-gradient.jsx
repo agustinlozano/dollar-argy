d3b6b66392b701d3aaf8fee94d0788afe3b453a6
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-25 17:12:29 -0300

    Update bg gradient

--- components/ui/bg-gradient.jsx
+++ components/ui/bg-gradient.jsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export const BgGradient = ({ className }) => (
   <div
     className={cn(
-      "absolute inset-0 z-10 h-full texture-fade w-full items-center opacity-65 [background:radial-gradient(125%_125%_at_50%_10%,#000_30%,#2c1810_100%)] rounded",
+      "absolute inset-0 z-10 h-full texture-fade w-full items-center opacity-95 [background:radial-gradient(125%_125%_at_50%_10%,#000_30%,#2c1810_100%)] rounded",
       className
     )}
   ></div>
