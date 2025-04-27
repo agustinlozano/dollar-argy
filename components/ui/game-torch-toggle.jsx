d0a3870d438288909795b4bc57678b5376370b75
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-27 16:58:27 -0300

    Add a new button to turn torch on/off

---
+++ components/ui/game-torch-toggle.jsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import { Flame } from "lucide-react";
+import { useGameStore } from "@/stores/useGameState";
+
+import "./game-torch-toggle.css";
+
+export function TorchToggleButton() {
+  const isTorchActive = useGameStore((state) => state.isTorchActive);
+  const toggleTorch = useGameStore((state) => state.toggleTorch);
+
+  return (
+    <div className="relative flex gap-x-4 justify-between items-center z-10 px-4">
+      <button
+        onClick={toggleTorch}
+        className={cn(
+          "torch-button-3d relative size-12 rounded-full border-2 border-dashed border-[#992200] bg-[#f86800] text-[#992200] flex items-center justify-center",
+          isTorchActive && "active"
+        )}
+        title={isTorchActive ? "Turn off torch" : "Turn on torch"}
+      >
+        <Flame className="z-10 size-6" />
+      </button>
+    </div>
+  );
+}
