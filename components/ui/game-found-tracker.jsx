2a9d06b107c085020a59e04ec9abf2c0297e529d
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-27 17:40:21 -0300

    Make FoundTracker layout relative instead of absolute

--- components/ui/game-found-tracker.jsx
+++ components/ui/game-found-tracker.jsx
@@ -9,29 +9,27 @@ export function FoundTracker() {
   const found = getQuantity("gold_coins");
 
   return (
-    <div className="absolute top-5 right-24">
-      <div className="relative flex gap-x-4 justify-between items-center z-10 px-4">
-        <div
-          className={cn(
-            "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
-            "rounded bg-accent-foreground/75 h-8 w-full flex items-center justify-center -z-10"
-          )}
-        />
-        <div
-          id="found-tacker-coin-icon"
-          className={cn(
-            "coin-3d relative size-12 rounded-full border-2 border-dashed border-yellow-700 bg-yellow-400 text-yellow-900 flex items-center justify-center"
-          )}
-        >
-          <DollarSign className="z-10" />
-        </div>
+    <div className="relative flex gap-x-4 justify-between items-center z-10 px-4">
+      <div
+        className={cn(
+          "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
+          "rounded bg-accent-foreground/75 h-8 w-full flex items-center justify-center -z-10"
+        )}
+      />
+      <div
+        id="found-tacker-coin-icon"
+        className={cn(
+          "coin-3d relative size-12 rounded-full border-2 border-dashed border-yellow-700 bg-yellow-400 text-yellow-900 flex items-center justify-center"
+        )}
+      >
+        <DollarSign className="z-10" />
+      </div>
 
-        <div
-          aria-label="amount"
-          className="flex items-center gap-2 font-bold text-accent text-lg"
-        >
-          <span>{found || 0}</span>
-        </div>
+      <div
+        aria-label="amount"
+        className="flex items-center gap-2 font-bold text-accent text-lg"
+      >
+        <span>{found || 0}</span>
       </div>
     </div>
   );
