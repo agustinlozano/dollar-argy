8b4a91626938dbe551a65316f4a0fb97a098325b
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-29 16:19:01 -0300

    Update TorchLight positions in Player component for improved lighting arrangement

--- components/game-player.jsx
+++ components/game-player.jsx
@@ -201,7 +201,8 @@ export const Player = forwardRef(function PlayerBill({ position }, ref) {
         {/* Add M16 weapon object */}
         <M16M1 position={[30, 25, 5]} rotation={[0, Math.PI / 2, 0]} />
         {/* Add torch light object */}
-        <TorchLight position={[40, 0, 0]} />
+        <TorchLight position={[0, 0, 30]} hideTorchObj />
+        <TorchLight position={[0, 0, -15]} hideTorchObj />
       </group>
       {/* </PivotControls> */}
     </group>
