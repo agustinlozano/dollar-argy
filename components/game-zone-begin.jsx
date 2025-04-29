6695f55079d9861958032102335db7ff36aed6f6
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-04-29 16:18:48 -0300

    Refactor FirstZone component to replace GameObjTorch with TorchLight, adjusting positions and rotations for improved lighting setup

--- components/game-zone-begin.jsx
+++ components/game-zone-begin.jsx
@@ -1,7 +1,6 @@
 import { useFrame } from "@react-three/fiber";
 import { FoundationsRockyZone } from "./game-zone-terrain-rocky-fundations";
-import { GameObjTorch } from "./game-obj-torch";
-import { PlayerTorchLight } from "./game-light-torch";
+import { TorchLight } from "./game-light-torch";
 
 export const FirstZone = ({ position }) => {
   // Animación o lógica para el Enclave (e.g., brillo en el mapa)
@@ -11,24 +10,13 @@ export const FirstZone = ({ position }) => {
 
   return (
     <group position={position}>
-      {/* Renderizar escenario */}
-      <GameObjTorch
-        initialPosition={[80, 60, 5]}
-        initialRotation={[Math.PI / 2, 0, 0]}
-      />
-      <GameObjTorch
-        initialPosition={[80, 220, 5]}
-        initialRotation={[Math.PI / 2, 0, 0]}
-      />
-      <GameObjTorch
-        initialPosition={[240, 60, 5]}
-        initialRotation={[Math.PI / 2, 0, 0]}
-      />
-      <GameObjTorch
-        initialPosition={[240, 220, 5]}
-        initialRotation={[Math.PI / 2, 0, 0]}
-      />
-      <FoundationsRockyZone position={[0, 0, -5]} gridSize={[3, 3]} />
+      <group position={[0, -150, 0]}>
+        <TorchLight position={[85, 60, 20]} rotation={[Math.PI / 2, 0, 0]} />
+        <TorchLight position={[85, 210, 20]} rotation={[Math.PI / 2, 0, 0]} />
+        <TorchLight position={[230, 60, 20]} rotation={[Math.PI / 2, 0, 0]} />
+        <TorchLight position={[230, 210, 20]} rotation={[Math.PI / 2, 0, 0]} />
+      </group>
+      <FoundationsRockyZone position={[0, -150, -5]} gridSize={[3, 3]} />
     </group>
   );
 };
