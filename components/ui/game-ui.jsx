4d40533d955393807b501e42902b760b9c5ef7a1
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-02 20:31:15 -0300

    Update GameUI by adding Dialogue component

--- components/ui/game-ui.jsx
+++ components/ui/game-ui.jsx
@@ -5,6 +5,7 @@ import { Inventory } from "./inventory/inventory";
 import { FoundTracker } from "./game-found-tracker";
 import { TorchToggleButton } from "./game-torch-toggle";
 import { InventoryToggleButton } from "./inventory/inventory-toggle-button";
+import { DialogueMenu } from "./dialogue-menu";
 
 import { useGameStore } from "@/stores/useGameState";
 import { useInventoryStore } from "@/stores/useInventoryState";
@@ -15,6 +16,7 @@ import * as defensiveSpells from "@/lib/spells.defensives";
 import * as buffSpells from "@/lib/spells.buffs";
 import * as consumables from "@/lib/items.consumable";
 import * as magicItems from "@/lib/items.magic";
+import { BgGradient } from "./bg-gradient";
 
 export function GameUI({
   items,
@@ -84,11 +86,26 @@ export function GameUI({
         />
       </div>
 
+      {/* Dialogue */}
+      {/* create a fixed position on the center of the screen */}
+      <div className="fixed inset-0 z-30 pointer-events-none flex items-center justify-center">
+        <div className="relative z-10">
+          <BgGradient className="-z-10" />
+          <div
+            role="presentation"
+            className="pointer-events-none absolute inset-0 opacity-55 z-10 texture-fade bg-[url('/textures/grunge-frame-3.1.png')] bg-cover bg-center rounded invert"
+          ></div>
+          <DialogueMenu />
+        </div>
+      </div>
+
       {/* Inventory Modal */}
       {isInventoryOpen && (
         <div className="fixed top-0 left-0 h-screen w-screen flex justify-center items-center z-50">
+          {/* Background overlay */}
           <div
-            className="absolute inset-0 bg-black/50"
+            role="presentation"
+            className="pointer-events-none absolute inset-0 bg-black/50"
             onClick={onInventoryToggle}
           ></div>
           <div className="relative z-10">
