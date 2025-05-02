fd2b943b9687b359d1e8977b35a584328d4b238f
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-02 20:29:24 -0300

    Add role attribute to BgGradient component for improved accessibility

--- components/ui/bg-gradient.jsx
+++ components/ui/bg-gradient.jsx
@@ -2,8 +2,9 @@ import { cn } from "@/lib/utils";
 
 export const BgGradient = ({ className }) => (
   <div
+    role="presentation"
     className={cn(
-      "absolute inset-0 z-10 h-full texture-fade w-full items-center opacity-95 [background:radial-gradient(125%_125%_at_50%_10%,#000_30%,#2c1810_100%)] rounded",
+      "pointer-events-none absolute inset-0 z-10 h-full texture-fade w-full items-center opacity-95 [background:radial-gradient(125%_125%_at_50%_10%,#000_30%,#2c1810_100%)] rounded",
       className
     )}
   ></div>
