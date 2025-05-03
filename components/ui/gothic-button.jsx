cdac1a95000eb0851e4fe52b311a39a009a4c48a
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-03 02:07:13 -0300

    Add `className` prop to `GothicButton` for additional styling

--- components/ui/gothic-button.jsx
+++ components/ui/gothic-button.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import s from "./gothic-button.module.css";
 
-export function GothicButton({ onClick, variant = "purple", children }) {
+export function GothicButton({
+  onClick,
+  variant = "purple",
+  children,
+  className,
+}) {
   return (
     <button
       className={cn(
         "font-cormorant relative px-4 py-2 border-b bg-purple-200 flex items-center justify-center",
         s.inventoryButton3d,
-        variant === "silver" && s.silver
+        variant === "silver" && s.silver,
+        className
       )}
       onClick={onClick}
     >
