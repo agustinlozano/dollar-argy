4b3b2b4f80ad8949eeb828d0860b0fbf66f61b7f
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-07 11:45:24 -0300

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  images: {
+    // remove in the future
+    domains: ["www.phbalcony.com"],
+  },
 };
 
 export default nextConfig;
