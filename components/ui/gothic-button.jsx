c13df160b83096b9bdb3d6f0f776d8fd203f1c3b
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-08 20:50:20 -0300

    Make gothic button smaller

--- components/ui/gothic-button.jsx
+++ components/ui/gothic-button.jsx
@@ -25,7 +25,7 @@ export function GothicButton({
       <audio ref={audioRef} src="/sounds/ui-feedback.wav" preload="auto" />
       <button
         className={cn(
-          "font-cormorant relative px-4 py-2 border-b bg-purple-200 flex items-center justify-center",
+          "font-cormorant relative px-4 py-1 border-b bg-purple-200 flex items-center justify-center",
           s.inventoryButton3d,
           variant === "silver" && s.silver,
           className
