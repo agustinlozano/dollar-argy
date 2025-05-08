c9cc97255e162c842187679bdfff089d9b11be7d
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-08 20:47:31 -0300

    Refactor InventoryToggleButton to use GothicButton component

--- components/ui/inventory/inventory-toggle-button.jsx
+++ components/ui/inventory/inventory-toggle-button.jsx
@@ -1,20 +1,11 @@
-import { cn } from "@/lib/utils";
-import { Backpack } from "lucide-react";
-
-import "./inventory-toggle-button.css";
+import { GothicButton } from "../gothic-button";
 
 export function InventoryToggleButton({ children, onClick, isOpen }) {
   return (
     <div className="relative flex gap-x-4 justify-between items-center z-10">
-      <button
-        onClick={onClick}
-        className={cn(
-          "inventory-button-3d font-cormorant relative px-4 py-2 border-b bg-purple-200 flex items-center justify-center",
-          isOpen && "active"
-        )}
-      >
+      <GothicButton onClick={onClick} isOpen={isOpen}>
         {children}
-      </button>
+      </GothicButton>
     </div>
   );
 }
