536a5d498c976da88e0db62777040627dd918fc9
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-13 19:32:58 -0300

    Just keep the second light

--- components/game-directional-light.jsx
+++ components/game-directional-light.jsx
@@ -5,8 +5,9 @@ import { enviroment } from "@/lib/env-vars";
 
 // Game environment lights
 export function PlayerDirectionalLight() {
+  const dirLightRef = useRef();
   const dirLightRef2 = useRef();
-  const targetRef = useRef();
+  // const targetRef = useRef();
 
   useHelper(
     enviroment === "development" && dirLightRef2 ? dirLightRef2 : null,
@@ -14,6 +15,13 @@ export function PlayerDirectionalLight() {
     100,
     "blue"
   );
+
+  // useHelper(
+  //   enviroment === "development" && dirLightRef ? dirLightRef : null,
+  //   THREE.DirectionalLightHelper,
+  //   100,
+  //   "blue"
+  // );
 
   useEffect(() => {
     if (dirLightRef2.current) {
@@ -24,27 +32,28 @@ export function PlayerDirectionalLight() {
 
   return (
     <>
-      <directionalLight
+      {/* <directionalLight
+        // ref={dirLightRef}
         position={[-200, -200, 400]}
         intensity={0.1}
         castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        shadow-mapSize-width={500}
+        shadow-mapSize-height={500}
         shadow-camera-left={-800}
         shadow-camera-right={800}
         shadow-camera-top={800}
         shadow-camera-bottom={-800}
         shadow-camera-near={50}
         shadow-camera-far={800}
-      />
+      /> */}
 
       <directionalLight
         ref={dirLightRef2}
         position={[200, 1000, 400]}
         intensity={0.1}
         castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        shadow-mapSize-width={500}
+        shadow-mapSize-height={500}
         shadow-camera-left={-800}
         shadow-camera-right={800}
         shadow-camera-top={800}
@@ -54,7 +63,7 @@ export function PlayerDirectionalLight() {
       />
 
       {/* Target object for the light */}
-      <object3D ref={targetRef} />
+      {/* <object3D ref={targetRef} /> */}
     </>
   );
 }
