656fa34fb81146ee2e3342f8f4a14174fe84cc6e
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-14 16:49:24 -0300

    Rename NPC obj component & update import

--- components/game-zone-begin.jsx
+++ components/game-zone-begin.jsx
@@ -1,6 +1,7 @@
 import { useFrame } from "@react-three/fiber";
 import { FoundationsRockyZone } from "./game-zone-terrain-rocky-fundations";
 import { TorchLight } from "./game-light-torch";
+import { HumanNPCObject } from "./game-obj-human-npc";
 
 export const FirstZone = ({ position }) => {
   // Animación o lógica para el Enclave (e.g., brillo en el mapa)
@@ -17,6 +18,7 @@ export const FirstZone = ({ position }) => {
         <TorchLight position={[230, 210, 20]} rotation={[Math.PI / 2, 0, 0]} />
       </group>
       <FoundationsRockyZone position={[0, -150, -5]} gridSize={[3, 3]} />
+      <HumanNPCObject position={[150, 0, 8]} rotation={[0, 0, Math.PI]} />
     </group>
   );
 };
