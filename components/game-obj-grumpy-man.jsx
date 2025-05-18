239dd9e637b363c83629bd3108ddb7c7ab25e51c
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-18 02:12:45 -0300

    Add GrumpyManObj component

---
+++ components/game-obj-grumpy-man.jsx
@@ -0,0 +1,26 @@
+import { useGLTF } from "@react-three/drei";
+
+export function GrumpyManObj(props) {
+  const { nodes, materials } = useGLTF("/characters/grumpy-man/model.glb");
+
+  if (!nodes || !materials) {
+    console.error("Model failed to load properly:", { nodes, materials });
+    return null;
+  }
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        geometry={nodes.model.geometry}
+        material={materials.CustomMaterial}
+        castShadow
+        scale={[45, 45, 45]} // Fit scale
+        position={[0, 0, 0]}
+        rotation={[0, Math.PI, 0]} // Rotate 180 degrees to face forward
+      />
+    </group>
+  );
+}
+
+// Precarga el modelo para evitar delays
+useGLTF.preload("/characters/grumpy-man-model/base.glb");
