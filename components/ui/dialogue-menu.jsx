d89fe1cd91270b8bd139167fee24073f5c3338a5
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-29 12:44:18 -0300

    Replace old dialogue menu for metallic one

--- components/ui/dialogue-menu.jsx
+++ components/ui/dialogue-menu.jsx
@@ -1,33 +1,71 @@
 import { CloseButton } from "./close-button";
+import { cn } from "@/lib/utils";
+import styles from "./metallic-dialogue-menu.module.css";
 
-export function DialogueMenu({
+export function MetallicDialogueMenu({
   dialogueText = '"La gente me rechaza, merezco una oportunidad digna."',
-  characterImage = "/characters/hornero-portrait.jpg",
+  characterImage = "/characters/hornero-portrait.png",
   characterName = "Hornero Peso Argentino 🇦🇷",
+  variant = "purple", // 'purple', 'silver' o 'brown'
   onClose,
+  onClick,
 }) {
   return (
-    <div className="relative h-[460px] w-[818px] border overflow-hidden p-20 z-30">
-      {onClose && <CloseButton onClick={onClose} />}
-      {/* Left side - Dialogue */}
-      <div className="relative flex h-full">
-        <div className="p-8 border w-md max-w-md">
-          <p className="text-primary/80 font-mono">{dialogueText}</p>
+    <div
+      className={cn(
+        styles.metallicContainer,
+        "",
+        variant === "silver"
+          ? styles.silver
+          : variant === "brown"
+          ? styles.brown
+          : styles.purple
+      )}
+    >
+      {onClose && (
+        <CloseButton onClick={onClose} className="absolute top-0 right-0" />
+      )}
+
+      <div className="relative flex h-full w-full z-10">
+        {/* Left side - Dialogue */}
+        <div
+          className={cn(
+            "px-8 pt-8 pb-4 border border-primary/20 w-xl flex flex-col justify-between",
+            styles.dialogueSection
+          )}
+        >
+          <div className="text-primary/90 font-mono font-light relative z-10 grow w-full text-xl max-h-80 overflow-auto">
+            <p>{dialogueText}</p>
+          </div>
+          <div className="flex justify-end">
+            <button
+              className="border text-xs px-2 py-1 flex items-center gap-x-2 bg-primary-foreground/75 hover:bg-primary-foreground transition-colors select-none"
+              onClick={onClick}
+              autoFocus
+            >
+              continuar ▶
+            </button>
+          </div>
         </div>
 
         {/* Right side - Character Image */}
-        <div className="flex flex-col items-center border bg-primary/10 w-52">
-          <div className="p-2 w-full">
+        <div
+          className={cn(
+            "flex flex-col items-center border border-primary/20 w-44",
+            styles.imageSection
+          )}
+        >
+          <div className="p-2 z-10 relative">
             <img
               src={characterImage || "/placeholder.svg"}
               alt={characterName}
-              className="w-full h-full border object-cover"
-              width={190.7}
-              height={190.7}
+              className="w-full border border-primary/20 select-none"
             />
           </div>
-          <div className="w-full text-center p-2 font">
-            <p className="font-cormorant text-lg">{characterName}</p>
+          <div className="w-full text-center p-2 z-10 relative">
+            <p className="font-cormorant text-xl text-white drop-shadow-md">
+              {characterName}
+            </p>
           </div>
         </div>
       </div>
