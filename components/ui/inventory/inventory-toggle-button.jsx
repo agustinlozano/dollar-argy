06873570a9d0a26e4efba5efa504dc7c7341b3eb
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2025-05-30 00:09:01 -0300

    Use amber variant for GothicButton

--- components/ui/inventory/inventory-toggle-button.jsx
+++ components/ui/inventory/inventory-toggle-button.jsx
@@ -3,7 +3,7 @@ import { GothicButton } from "../gothic-button";
 export function InventoryToggleButton({ children, onClick, isOpen }) {
   return (
     <div className="relative flex gap-x-4 justify-between items-center z-10">
-      <GothicButton onClick={onClick} isOpen={isOpen}>
+      <GothicButton variant="amber" onClick={onClick} isOpen={isOpen}>
         {children}
       </GothicButton>
     </div>
